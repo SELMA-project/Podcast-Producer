52dd8b493c5156bc49335647e766beb566cf7fe7
Andy Giefer <andreas.giefer@dw.com>
2022-02-04 12:36:11 +0100

    Initial Commit

---
+++ Shared/Podcast_ProducerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Podcast_ProducerApp.swift
+//  Shared
+//
+//  Created by Andy Giefer on 04.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Podcast_ProducerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
