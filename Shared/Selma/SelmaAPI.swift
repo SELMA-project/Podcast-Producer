baa4b854d9f3069ff58d5aa2e3d9719e51354350
Andy Giefer <andreas.giefer@dw.com>
2022-08-16 10:04:45 +0200

    SelmaAPI separated out

---
+++ Shared/Selma/SelmaAPI.swift
@@ -0,0 +1,59 @@
+//
+//  SelmaAPI.swift
+//  Podcast Producer
+//
+//  Created by Andy Giefer on 16.08.22.
+//
+
+import Foundation
+
+class SelmaAPI {
+    
+    // singleton
+    static var shared = SelmaAPI()
+    
+    func renderAudio(speakerName: String, text: String) async -> Data? {
+        
+        // path on server
+        let path = "/x:selmaproject:tts:777:5002/api/tts"
+        
+        // query parameters
+        let textQueryItem = URLQueryItem(name: "text", value: text)
+        let speakerQueryItem = URLQueryItem(name: "speaker_id", value: speakerName)
+        let queryItems = [textQueryItem, speakerQueryItem]
+        
+        // endpoint
+        let uc0endPoint = UC0Endpoint(path: path, queryItems: queryItems)
+        
+        // result
+        var data: Data?
+        
+        if let url = uc0endPoint.url {
+            
+            do {
+                (data, _) = try await URLSession.shared.data(from: url)
+            } catch {
+                print("Download error: \(error)")
+            }
+        }
+        
+        return data
+    }
+}
+
+struct UC0Endpoint {
+    
+    let path: String
+    let queryItems: [URLQueryItem]
+    
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "http"
+        components.host = "87.110.211.231"
+        components.port = 10100
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components.url
+    }
+}
--- Shared/Selma/SelmaViewModel.swift
+++ Shared/Selma/SelmaViewModel.swift
@@ -32,69 +32,30 @@ import Foundation
 //
 //    return endTime - startTime
 
+@MainActor
 class SelmaViewModel: ObservableObject {
-
+    
     @Published var statusMessage: String = ""
     
     func testRender() async {
-
+        
         let speakerName = "leila endruweit"
         let text = "Olá, hoje é quinta-feira, três de setembro de 2020."
-
-        await renderAudio(speakerName: speakerName, text: text)
-    }
-
-    func renderAudio(speakerName: String, text: String) async {
         
-        DispatchQueue.main.async {
-            self.statusMessage = "Rendering audio..."
+        self.statusMessage = "Rendering audio..."
+        
+        let data = await SelmaAPI.shared.renderAudio(speakerName: speakerName, text: text)
+        
+        var message = "Error while rendering audio on the server."
+        if let data = data {
+            message = "Data received: \(data.description)"
         }
         
-        // path on server
-        let path = "/x:selmaproject:tts:777:5002/api/tts"
-        
-        // query parameters
-        let textQueryItem = URLQueryItem(name: "text", value: text)
-        let speakerQueryItem = URLQueryItem(name: "speaker_id", value: speakerName)
-        let queryItems = [textQueryItem, speakerQueryItem]
-        
-        // endpoint
-        let uc0endPoint = UC0Endpoint(path: path, queryItems: queryItems)
-        
-        if let url = uc0endPoint.url {
-              
-              do {
-                  let (data, _) = try await URLSession.shared.data(from: url)
-                  let message = "Data received: \(data.description)"
-                  DispatchQueue.main.async {
-                      self.statusMessage = message
-                      print(message)
-                  }
-
-              } catch {
-                  print("Download error: \(error)")
-              }
-          }
-
+        self.statusMessage = message
     }
+    
+
     
 }
 
 
-
-struct UC0Endpoint {
-    
-    let path: String
-    let queryItems: [URLQueryItem]
-    
-    var url: URL? {
-        var components = URLComponents()
-        components.scheme = "http"
-        components.host = "87.110.211.231"
-        components.port = 10100
-        components.path = path
-        components.queryItems = queryItems
-
-        return components.url
-    }
-}
