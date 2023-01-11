c2904d41e254784bf9e40a417a0ad7380e1e736b
Andy Giefer <andreas.giefer@dw.com>
2023-01-11 11:12:37 +0100

    Removed BuilduingStructure legacy Code from ViewModel

--- Shared/EpisodeViewModel.swift
+++ Shared/EpisodeViewModel.swift
@@ -259,228 +259,228 @@ class EpisodeViewModel: ObservableObject {
 
 
 
-// MARK: - Legacy BuildingBlock Code
-// this should be obsolete soon
-extension EpisodeViewModel {
-    
-//    /// Called when 'Create Audio' button is pressed in AudioRenderView
-//    func buildAudio() async {
-//  
-//        // create entire episode
-//        let episode = self.availableEpisodes[chosenEpisodeIndex]
-//        episodeUrl = await AudioManager.shared.createAudioEpisodeBasedOnEpisode(episode, selectedSectionIndex: nil)
-//        print("Audio file saved here: \(String(describing: episodeUrl))")
+//// MARK: - Legacy BuildingBlock Code
+//// this should be obsolete soon
+//extension EpisodeViewModel {
+//    
+////    /// Called when 'Create Audio' button is pressed in AudioRenderView
+////    func buildAudio() async {
+////  
+////        // create entire episode
+////        let episode = self.availableEpisodes[chosenEpisodeIndex]
+////        episodeUrl = await AudioManager.shared.createAudioEpisodeBasedOnEpisode(episode, selectedSectionIndex: nil)
+////        print("Audio file saved here: \(String(describing: episodeUrl))")
+////        
+////        // publish existance of the new audio URL in viewmodel
+////        episodeAvailable = true
+////    }
+//    
+//    /// Remove all rendered audio pointed to by the episode structure
+//    /// Thids might be obsolte, as there is no caller
+//    private func removeAudio(inEpisodeStructure episodeStructure: [BuildingBlock]) {
 //        
-//        // publish existance of the new audio URL in viewmodel
-//        episodeAvailable = true
-//    }
-    
-    /// Remove all rendered audio pointed to by the episode structure
-    /// Thids might be obsolte, as there is no caller
-    private func removeAudio(inEpisodeStructure episodeStructure: [BuildingBlock]) {
-        
-        var newEpisodeStructure = [BuildingBlock]()
-        
-        for segment in episodeStructure {
-            
-            // make a copy
-            let newSegment = segment
-            
-            if let audioURL = newSegment.audioURL {
-                
-                // remove audio
-                try? FileManager.default.removeItem(at: audioURL)
-                
-                // mark audio as not rendered
-                //newSegment.audioIsRendered = false
-            }
-            
-            // store in new episode structure
-            newEpisodeStructure.append(newSegment)
-        }
-        
-        // update structure
-        self.episodeStructure = newEpisodeStructure
-        
-    }
-    
-    
-    func buildEpisodeStructure() {
-
-        // result
-        var structure = [BuildingBlock]()
-        
-        // episode to build
-       // let chosenEpisode = availableEpisodes[chosenEpisodeIndex]
-                
-        for episodeSection in chosenEpisode.sections {
-
-            // calculate the section's audio URL the section type and the text
-            let episodeSectionText = chosenEpisode.replaceTokens(inText: episodeSection.rawText)
-            //let episodeSectionType = episodeSection.type
-            let textAudioURL = chosenEpisode.textAudioURL(forSection: episodeSection)
-            
-            switch episodeSection.type {
-            case .standard:
-                let name = episodeSection.name
-                
-                // FIXME: this should become obsolete
-                // deduce block identifier from name
-                let blockIdentifier: BlockIdentifier
-                switch name {
-                
-                case "Introduction":
-                    blockIdentifier = .introduction
-                
-                case "Epilog":
-                    blockIdentifier = .epilogue
-                
-                default:
-                    blockIdentifier = .unknown
-                }
-                
-                // calculate the section's audio URL the section type and the text
-                //let textAudioURL = chosenEpisode.textAudioURL(forSectionType: episodeSectionType, textContent: episodeSectionText)
-                
-                let buildingBlock = BuildingBlock(blockIdentifier: blockIdentifier, audioURL: textAudioURL, text: episodeSectionText)
-                structure.append(buildingBlock)
-                
-            case .headlines:
-                
-                // add headlines text if present
-                if episodeSectionText.count > 0 {
-                    //let textAudioURL = episodeSection.textAudioURL
-                    let buildingBlock = BuildingBlock(blockIdentifier: .introduction, audioURL: textAudioURL, text: episodeSectionText)
-                    structure.append(buildingBlock)
-                }
-                
-                for (index, story) in chosenEpisode.stories.enumerated() {
-                    if story.usedInIntroduction || !chosenEpisode.restrictHeadlinesToHighLights {
-                        let storyHeadline = story.headline
-                        let proposedHeadlineAudioUrl = chosenEpisode.headlineAudioURL(forStory: story)
-                        let buildingBlock = BuildingBlock(blockIdentifier: .headline, subIndex: index, audioURL: proposedHeadlineAudioUrl, text: storyHeadline, highlightInSummary: story.usedInIntroduction)
-                        structure.append(buildingBlock)
-                    }
-                }
-
-            case .stories:
-                
-                // add headlines text if present
-                if episodeSectionText.count > 0 {
-                    //let textAudioURL = episodeSection.textAudioURL
-                    let buildingBlock = BuildingBlock(blockIdentifier: .introduction, audioURL: textAudioURL, text: episodeSectionText)
-                    structure.append(buildingBlock)
-                }
-                
-                for (index, story) in chosenEpisode.stories.enumerated() {
-                    let storyText = story.storyText
-                    let proposedTextAudioUrl = chosenEpisode.storyTextAudioURL(forStory: story)
-                    let buildingBlock = BuildingBlock(blockIdentifier: .story, subIndex: index, audioURL: proposedTextAudioUrl, text: storyText)
-                    structure.append(buildingBlock)
-                }
-                
-            }
-        
-        }
-        
-        // publish
-        self.episodeStructure = structure
-    }
-
-
-    func renderEpisodeStructure() async {
-        
-        for (index, audioSegment) in episodeStructure.enumerated() {
-            print("Cancel status: \(Task.isCancelled)")
-            print("Rendering: \(index) -> \(audioSegment.blockIdentifier.rawValue)")
-  
-            // render audio -> this updates the audio segment's property 'audioISRendered'
-            let updatedAudioSegment = await renderAudioInBuildingBlock(audioSegment)
-            
-            // replace original block
-            episodeStructure[index] = updatedAudioSegment
-                        
-        }
-        
-    }
-        
-    
-    /// Synthesizes audio for provided building block.
-    private func renderAudioInBuildingBlock(_ buildingBlock:  BuildingBlock) async -> BuildingBlock  {
-        
-        // copy original building block to be able to update the audioURL
-        let updatedBuildingBlock = buildingBlock
-        
-        // early exit if we don't have an audioURL (this should not happen, as the URL is proposed by the episode in buildEpisodeStructure()
-        guard let audioURL = buildingBlock.audioURL else {return updatedBuildingBlock}
-        
-        // the text to render
-        let text = buildingBlock.text
-        
-        // the speaker identifier
-        let podcastVoice = chosenEpisode.podcastVoice
-        
-        // render audio if it does not yet exist
-        var success = true
-        if !fileExists(atURL: audioURL) {
-            success = await AudioManager.shared.synthesizeSpeech(podcastVoice: podcastVoice, text: text, toURL: audioURL)
-        }
-        
-        if !success {
-            print("No audio data available.")
-        }
-
-//        if success {
-//            //episodeStructure[index].audioURL = audioURL
-//            //updatedBuildingBlock.audioURL = audioURL
-//            //updatedBuildingBlock.audioIsRendered = true
-//        } else {
-//
+//        var newEpisodeStructure = [BuildingBlock]()
+//        
+//        for segment in episodeStructure {
+//            
+//            // make a copy
+//            let newSegment = segment
+//            
+//            if let audioURL = newSegment.audioURL {
+//                
+//                // remove audio
+//                try? FileManager.default.removeItem(at: audioURL)
+//                
+//                // mark audio as not rendered
+//                //newSegment.audioIsRendered = false
+//            }
+//            
+//            // store in new episode structure
+//            newEpisodeStructure.append(newSegment)
 //        }
-        
-        return updatedBuildingBlock
-    }
-    
-    
-    
-    func playButtonPressed(forSegment audioSegment: BuildingBlock) async {
-
-        // find index of given audioSegment in array
-        let currentIndex = episodeStructure.firstIndex { segment in
-            segment.id == audioSegment.id
-        }
-        
-        // early return if no index was found (should not happen)
-        guard let currentIndex = currentIndex else {return}
-        
-        // early exit if no audio data is available (should not happen)
-        guard let audioUrl = audioSegment.audioURL else {return}
-
-        // in any case, stop the currently played audio
-        AudioManager.shared.stopAudio()
-        
-        // currently not playng, so we want to play
-        if audioSegment.isPlaying == false {
-            
-            // switch all audioSegments to 'off' - except the one with the current index
-            for (index, _) in episodeStructure.enumerated() {
-                episodeStructure[index].isPlaying = currentIndex == index ? true : false
-            }
-            
-            // switch to 'playing'
-            //episodeStructure[index].isPlaying = true
-            
-            // play segment
-            await AudioManager.shared.playAudio(audioUrl: audioUrl)
-            
-            // when returning, switch to 'not playing'
-            episodeStructure[currentIndex].isPlaying = false
-            
-        } else { // segment is currently playing
-            
-            // switch to 'not playing'
-            episodeStructure[currentIndex].isPlaying = false
-        }
-        
-    }
-}
+//        
+//        // update structure
+//        self.episodeStructure = newEpisodeStructure
+//        
+//    }
+//    
+//    
+//    func buildEpisodeStructure() {
+//
+//        // result
+//        var structure = [BuildingBlock]()
+//        
+//        // episode to build
+//       // let chosenEpisode = availableEpisodes[chosenEpisodeIndex]
+//                
+//        for episodeSection in chosenEpisode.sections {
+//
+//            // calculate the section's audio URL the section type and the text
+//            let episodeSectionText = chosenEpisode.replaceTokens(inText: episodeSection.rawText)
+//            //let episodeSectionType = episodeSection.type
+//            let textAudioURL = chosenEpisode.textAudioURL(forSection: episodeSection)
+//            
+//            switch episodeSection.type {
+//            case .standard:
+//                let name = episodeSection.name
+//                
+//                // FIXME: this should become obsolete
+//                // deduce block identifier from name
+//                let blockIdentifier: BlockIdentifier
+//                switch name {
+//                
+//                case "Introduction":
+//                    blockIdentifier = .introduction
+//                
+//                case "Epilog":
+//                    blockIdentifier = .epilogue
+//                
+//                default:
+//                    blockIdentifier = .unknown
+//                }
+//                
+//                // calculate the section's audio URL the section type and the text
+//                //let textAudioURL = chosenEpisode.textAudioURL(forSectionType: episodeSectionType, textContent: episodeSectionText)
+//                
+//                let buildingBlock = BuildingBlock(blockIdentifier: blockIdentifier, audioURL: textAudioURL, text: episodeSectionText)
+//                structure.append(buildingBlock)
+//                
+//            case .headlines:
+//                
+//                // add headlines text if present
+//                if episodeSectionText.count > 0 {
+//                    //let textAudioURL = episodeSection.textAudioURL
+//                    let buildingBlock = BuildingBlock(blockIdentifier: .introduction, audioURL: textAudioURL, text: episodeSectionText)
+//                    structure.append(buildingBlock)
+//                }
+//                
+//                for (index, story) in chosenEpisode.stories.enumerated() {
+//                    if story.usedInIntroduction || !chosenEpisode.restrictHeadlinesToHighLights {
+//                        let storyHeadline = story.headline
+//                        let proposedHeadlineAudioUrl = chosenEpisode.headlineAudioURL(forStory: story)
+//                        let buildingBlock = BuildingBlock(blockIdentifier: .headline, subIndex: index, audioURL: proposedHeadlineAudioUrl, text: storyHeadline, highlightInSummary: story.usedInIntroduction)
+//                        structure.append(buildingBlock)
+//                    }
+//                }
+//
+//            case .stories:
+//                
+//                // add headlines text if present
+//                if episodeSectionText.count > 0 {
+//                    //let textAudioURL = episodeSection.textAudioURL
+//                    let buildingBlock = BuildingBlock(blockIdentifier: .introduction, audioURL: textAudioURL, text: episodeSectionText)
+//                    structure.append(buildingBlock)
+//                }
+//                
+//                for (index, story) in chosenEpisode.stories.enumerated() {
+//                    let storyText = story.storyText
+//                    let proposedTextAudioUrl = chosenEpisode.storyTextAudioURL(forStory: story)
+//                    let buildingBlock = BuildingBlock(blockIdentifier: .story, subIndex: index, audioURL: proposedTextAudioUrl, text: storyText)
+//                    structure.append(buildingBlock)
+//                }
+//                
+//            }
+//        
+//        }
+//        
+//        // publish
+//        self.episodeStructure = structure
+//    }
+//
+//
+//    func renderEpisodeStructure() async {
+//        
+//        for (index, audioSegment) in episodeStructure.enumerated() {
+//            print("Cancel status: \(Task.isCancelled)")
+//            print("Rendering: \(index) -> \(audioSegment.blockIdentifier.rawValue)")
+//  
+//            // render audio -> this updates the audio segment's property 'audioISRendered'
+//            let updatedAudioSegment = await renderAudioInBuildingBlock(audioSegment)
+//            
+//            // replace original block
+//            episodeStructure[index] = updatedAudioSegment
+//                        
+//        }
+//        
+//    }
+//        
+//    
+//    /// Synthesizes audio for provided building block.
+//    private func renderAudioInBuildingBlock(_ buildingBlock:  BuildingBlock) async -> BuildingBlock  {
+//        
+//        // copy original building block to be able to update the audioURL
+//        let updatedBuildingBlock = buildingBlock
+//        
+//        // early exit if we don't have an audioURL (this should not happen, as the URL is proposed by the episode in buildEpisodeStructure()
+//        guard let audioURL = buildingBlock.audioURL else {return updatedBuildingBlock}
+//        
+//        // the text to render
+//        let text = buildingBlock.text
+//        
+//        // the speaker identifier
+//        let podcastVoice = chosenEpisode.podcastVoice
+//        
+//        // render audio if it does not yet exist
+//        var success = true
+//        if !fileExists(atURL: audioURL) {
+//            success = await AudioManager.shared.synthesizeSpeech(podcastVoice: podcastVoice, text: text, toURL: audioURL)
+//        }
+//        
+//        if !success {
+//            print("No audio data available.")
+//        }
+//
+////        if success {
+////            //episodeStructure[index].audioURL = audioURL
+////            //updatedBuildingBlock.audioURL = audioURL
+////            //updatedBuildingBlock.audioIsRendered = true
+////        } else {
+////
+////        }
+//        
+//        return updatedBuildingBlock
+//    }
+//    
+//    
+//    
+//    func playButtonPressed(forSegment audioSegment: BuildingBlock) async {
+//
+//        // find index of given audioSegment in array
+//        let currentIndex = episodeStructure.firstIndex { segment in
+//            segment.id == audioSegment.id
+//        }
+//        
+//        // early return if no index was found (should not happen)
+//        guard let currentIndex = currentIndex else {return}
+//        
+//        // early exit if no audio data is available (should not happen)
+//        guard let audioUrl = audioSegment.audioURL else {return}
+//
+//        // in any case, stop the currently played audio
+//        AudioManager.shared.stopAudio()
+//        
+//        // currently not playng, so we want to play
+//        if audioSegment.isPlaying == false {
+//            
+//            // switch all audioSegments to 'off' - except the one with the current index
+//            for (index, _) in episodeStructure.enumerated() {
+//                episodeStructure[index].isPlaying = currentIndex == index ? true : false
+//            }
+//            
+//            // switch to 'playing'
+//            //episodeStructure[index].isPlaying = true
+//            
+//            // play segment
+//            await AudioManager.shared.playAudio(audioUrl: audioUrl)
+//            
+//            // when returning, switch to 'not playing'
+//            episodeStructure[currentIndex].isPlaying = false
+//            
+//        } else { // segment is currently playing
+//            
+//            // switch to 'not playing'
+//            episodeStructure[currentIndex].isPlaying = false
+//        }
+//        
+//    }
+//}
