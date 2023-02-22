2c49a9090a8c499cc644b08490567ddcb40a3277
Andy Giefer <andreas.giefer@dw.com>
2023-02-22 09:37:40 +0100

    Prepared macOS copy of EpisodeEditorView

---
+++ Shared/Extensions/View+if.swift
@@ -0,0 +1,20 @@
+//
+//  View+if.swift
+//  Podcast Creator (iOS)
+//
+//  Created by Andy Giefer on 22.02.23.
+//
+
+import SwiftUI
+
+//extension View {
+//    @ViewBuilder
+//    func `if`<Content: View>(_ conditional: Bool,  @ViewBuilder content: (Self) -> Content) -> some View {
+//        if conditional {
+//            content(self)
+//        }
+//        else {
+//            self
+//        }
+//    }
+//}
