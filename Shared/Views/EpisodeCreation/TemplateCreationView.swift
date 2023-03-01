13664121bf333cfdc1de085a0ec5d16dcfcbfd6b
Andy Giefer <andreas.giefer@dw.com>
2023-03-01 11:35:54 +0100

    Created new folders and files

---
+++ Shared/Views/EpisodeCreation/TemplateCreationView.swift
@@ -0,0 +1,94 @@
+//
+//  TemplateCreationView.swift
+//  Podcast Creator
+//
+//  Created by Andy Giefer on 28.10.22.
+//
+
+import SwiftUI
+
+struct TemplateCreationView: View {
+    
+    @State var language: LanguageManager.Language = .english
+    @State var templateIndex: Int = 0
+    
+    @Environment(\.dismiss) var dismissAction
+    @EnvironmentObject var episodeViewModel: EpisodeViewModel
+    
+    var templates: [EpisodeTemplate] {
+        let templates = TemplateManager.shared.availableTemplates(forLanguage: language)
+            
+        return templates
+    }
+
+    var body: some View {
+        
+        NavigationStack {
+            
+            VStack(alignment: .leading) {
+                
+                Text("Create a new episode based on a template.")
+                    .font(.callout)
+                    .padding()
+                
+                
+                Form {
+                    
+                    Section("Specify Template") {
+                        Picker("Language", selection: $language) {
+                            ForEach(LanguageManager.Language.allCases, id: \.self) {language in
+                                Text(language.displayName)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .onChange(of: language) {_ in 
+                            templateIndex = 0
+                        }
+                        
+                        Picker("Template", selection: $templateIndex) {
+                            ForEach(0..<templates.count, id: \.self) { index in
+                                Text(templates[index].name)
+                            }
+                        }.pickerStyle(.menu)
+                        
+                        HStack {
+                            Text("Narrator")
+                            Spacer()
+                            TextField("Name", text: $episodeViewModel.newTemplateNarratorName)
+                                .multilineTextAlignment(.trailing)
+                        }
+                    }
+                    
+                    
+                    HStack {
+                        Spacer()
+                        Button("Create") {
+                            // create episode based on currently selected template
+                            let template = templates[templateIndex]
+                            episodeViewModel.addEpisode(basedOnTemplate: template)
+                            dismissAction()
+                        }
+                        Spacer()
+                    }
+                    
+                }
+            }
+            .toolbar {
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismissAction()
+                    }
+                }
+            }
+            .navigationTitle("New Episode")
+        }
+    }
+}
+
+struct TemplateCreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplateCreationView()
+            .environmentObject(EpisodeViewModel())
+    }
+}
---
+++ macOS/Views/EpisodeCreation/TemplateCreationView.swift
@@ -0,0 +1,90 @@
+//
+//  TemplateCreationView.swift
+//  Podcast Creator
+//
+//  Created by Andy Giefer on 28.10.22.
+//
+
+import SwiftUI
+
+struct TemplateCreationView: View {
+    
+    @State var language: LanguageManager.Language = .english
+    @State var templateIndex: Int = 0
+    
+    @Environment(\.dismiss) var dismissAction
+    @EnvironmentObject var episodeViewModel: EpisodeViewModel
+    
+    var templates: [EpisodeTemplate] {
+        let templates = TemplateManager.shared.availableTemplates(forLanguage: language)
+            
+        return templates
+    }
+
+    var body: some View {
+        
+
+            
+        VStack(alignment: .leading) {
+            
+            Text("New Episode").font(.title)
+            
+            Text("Create a new episode based on a template.")
+                .font(.callout)
+            
+            
+            
+            Form {
+                
+                Picker("Language", selection: $language) {
+                    ForEach(LanguageManager.Language.allCases, id: \.self) {language in
+                        Text(language.displayName)
+                    }
+                }
+                .pickerStyle(.menu)
+                .onChange(of: language) {_ in
+                    templateIndex = 0
+                }
+                
+                Picker("Template", selection: $templateIndex) {
+                    ForEach(0..<templates.count, id: \.self) { index in
+                        Text(templates[index].name)
+                    }
+                }.pickerStyle(.menu)
+                
+                TextField("Narrator", text: $episodeViewModel.newTemplateNarratorName)
+                    .multilineTextAlignment(.trailing)
+                
+            }
+            .padding([.top,. bottom])
+            
+            Divider()
+            
+            HStack {
+                Spacer()
+                Button("Cancel") {
+                    dismissAction()
+                }
+                Button("Create") {
+                    // create episode based on currently selected template
+                    let template = templates[templateIndex]
+                    episodeViewModel.addEpisode(basedOnTemplate: template)
+                    dismissAction()
+                }
+            }
+            
+            
+        }
+        .padding()
+        .frame(width:400)
+
+        
+    }
+}
+
+struct TemplateCreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplateCreationView()
+            .environmentObject(EpisodeViewModel())
+    }
+}
