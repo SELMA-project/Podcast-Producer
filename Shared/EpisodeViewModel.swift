d69cd95c02a62157a5f6394fdc91582c5673849c
Andy Giefer <andreas.giefer@dw.com>
2023-03-31 18:27:38 +0200

    Add remote DWUtilitites

--- Shared/EpisodeViewModel.swift
+++ Shared/EpisodeViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Combine
 import SwiftUI
+import DWUtilities
 
 @MainActor
 class EpisodeViewModel: ObservableObject {
--- macOS/Views/EpisodeEditor/EpisodeEditorStoriesListView.swift
+++ macOS/Views/EpisodeEditor/EpisodeEditorStoriesListView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import DWUtilities
 
 struct EpisodeEditorStoriesListView: View {
     
