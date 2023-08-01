9a9d9dc5083e8803743d2e7bf5ff25eb0ad37b56
Andy Giefer <andreas.giefer@dw.com>
2023-08-01 11:44:09 +0200

    Added Alpaca summarisation

--- macOS/Views/EpisodeEditor/EpisodeEditorStoryView.swift
+++ macOS/Views/EpisodeEditor/EpisodeEditorStoryView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import SelmaKit
 
 struct EpisodeEditorStoryView: View {
     
     @Binding var story: Story
 
     @EnvironmentObject var episodeViewModel: EpisodeViewModel
+    
+    @State var engineIsProcessing = false
     
     /// Gets called when 'Summarize button is pressed.
     private func summarize() {
@@ -29,21 +32,53 @@ struct EpisodeEditorStoryView: View {
         // read temperature defaults
         guard let summarizationPrompt = UserDefaults.standard.string(forKey: Constants.userDefaultsSummarizationPrompt) else {return}
         
+        // text to summarise
+        let storyText = story.storyText
+        
         switch selectedEngine {
         case .titleAndTeaser:
-            summarizeUsingTitleAndTeaser()
+            summarizeUsingTitleAndTeaser(storyText)
         case .alpaca:
-            break
+            summarizeWithAlpaca(storyText, prompt: summarizationPrompt, maxNumberOfTokens: maxNumberOfTokens)
         case .openAI:
             break
         }
     }
     
+    func summarizeWithAlpaca(_ incomingText: String, prompt promptText: String, maxNumberOfTokens: Int) {
+        
+        // access to API
+        let selmaAPI = SelmaAPI()
+
+        // disable interaction
+        engineIsProcessing = true
+        
+        Task {
+            
+            let stream = await selmaAPI.sendAlpacaRequest(predictionLength: maxNumberOfTokens, prompt: promptText, context: incomingText)
+            
+            var summary = ""
+            
+            if let stream {
+                for try await character in stream.characters {
+                    //print("\(Date())\tReceived character: \(character)")
+                    summary.append(character)
+                }
+                story.storyText = summary
+            } else {
+                print("<SELMA Server error.>")
+            }
+            
+            // re-enable interaction
+            engineIsProcessing = false
+        }
+    }
+    
     /// Summarizes the story by reducing it to the first two paragraphs.
-    private func summarizeUsingTitleAndTeaser() {
+    private func summarizeUsingTitleAndTeaser(_ incomingText: String) {
         
         // split into paragraphs
-        let paragraphs = story.storyText.split(separator: "\n")
+        let paragraphs = incomingText.split(separator: "\n")
         
         // use the first two non-empty paragraphs
         // these are the headline and the teaser text
@@ -67,10 +102,19 @@ struct EpisodeEditorStoryView: View {
                     
                         HStack {
                             Text("Text").font(.title2)
+                            
                             Spacer()
+                            
+                            // Progress View
+                            ProgressView()
+                                .scaleEffect(0.5, anchor: .center)
+                                .opacity(engineIsProcessing ? 1 : 0)
+                            
+                            // Summarization button
                             Button("Summarize") {
                                 summarize()
                             }
+                            .disabled(engineIsProcessing)
                         }.padding(.top)
                         
                         TextEditor(text: $story.storyText).font(.title3)
